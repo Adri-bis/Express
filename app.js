038fb10de7bb509bc694938b8e81b8f7fb5727fe
Adri-bis <adrien.ma.16@hotmail.fr>
2023-11-27 11:04:34 +0100

    feat: finish 01-1

--- app.js
+++ app.js
@@ -1,110 +1,75 @@
 const express = require("express");
 
-
 const app = express();
 
-
-const port = 5000;
-
-
-const movies = [
-
-    {
-
-        id: 1,
-
-        title: "Citizen Kane",
-
-        director: "Orson Wells",
-
-        year: "1941",
-
-        color: false,
-
-        duration: 120,
-
-    },
-
-    {
-
-        id: 2,
-
-        title: "The  Godfather",
-
-        director: "Francis Ford Coppola",
-
-        year: "1972",
-
-        color: true,
-
-        duration: 180,
-
-    },
-
-    {
-
-        id: 3,
-
-        title: "Pulp Fiction",
-
-        director: "Quentin Tarantino",
-
-        year: "1994",
-
-        color: true,
-
-        duration: 180,
-
-    },
-
-];
-
 const welcome = (req, res) => {
-
-    res.send("Welcome to my favourite movie list");
-
+  res.send("Welcome to my favourite movie list");
 };
-
 
 app.get("/", welcome);
 
+const movies = [
+  {
+    id: 1,
 
-const allMovie = (req, res) => {
+    title: "Citizen Kane",
 
-    res.status(200).json({ movies });
+    director: "Orson Wells",
 
+    year: "1941",
+
+    color: false,
+
+    duration: 120,
+  },
+
+  {
+    id: 2,
+
+    title: "The  Godfather",
+
+    director: "Francis Ford Coppola",
+
+    year: "1972",
+
+    color: true,
+
+    duration: 180,
+  },
+
+  {
+    id: 3,
+
+    title: "Pulp Fiction",
+
+    director: "Quentin Tarantino",
+
+    year: "1994",
+
+    color: true,
+
+    duration: 180,
+  },
+];
+
+const getMovies = (req, res) => {
+  res.json(movies);
 };
 
-app.get("/api/movies", allMovie);
+app.get("/api/movies", getMovies);
 
+const getMovieById = (req, res) => {
+  const id = parseInt(req.params.id);
 
+  const movie = movies.find((movie) => movie.id === id);
 
-const movieId = (req, res) => {
-
-    const movid = parseInt(req.params.id)
-    const idchoi = movies.find((el) => el.id === movid)
-
-    if (idchoi) {
-        res.status(200).json({ idchoi })
-    } else {
-        res.status(404).send('not found')
-    }
+  if (movie != null) {
+    res.json(movie);
+  } else {
+    res.sendStatus(404);
+  }
 };
 
-app.get("/api/movies/:id", movieId)
+app.get("/api/movies/:id", getMovieById);
 
-
-
-app.listen(port, (err) => {
-
-    if (err) {
-
-        console.error("Something bad happened");
-
-    } else {
-
-        console.log(`Server is listening on ${port}`);
-
-    }
-
-});
+module.exports = app;
--- myapi.js
+++ myapi.js
@@ -1,150 +1,141 @@
-const express = require("express");
-const app = express();
-const port = 3310;
+// const express = require("express");
+// const app = express();
+// const port = 3310;
 
-const licorne = [
-    {
-        id: 1,
-        name: "lola",
-        age: 254,
-        color: "rose",
-    },
-    {
-        id: 2,
-        name: "paola",
-        age: 196,
-        color: "rose",
-    },
-    {
-        id: 3,
-        name: "resti",
-        age: 170,
-        color: "rose",
-    },
-    {
-        id: 4,
-        name: "zoe",
-        age: 85,
-        color: "blue",
-    },
-    {
-        id: 5,
-        name: "jouli",
-        age: 200,
-        color: "rose",
-    },
-    {
-        id: 6,
-        name: "fleur",
-        age: 186,
-        color: "rose",
-    },
-    {
-        id: 7,
-        name: "rose",
-        age: 128,
-        color: "blue",
-    },
-    {
-        id: 8,
-        name: "prune",
-        age: 124,
-        color: "blue",
-    },
-    {
-        id: 9,
-        name: "agdabel",
-        age: 654,
-        color: "blue",
-    },
-    {
-        id: 10,
-        name: "agdabel",
-        age: 654,
-        color: "blue",
-    },
-    {
-        id: 11,
-        name: "agdabel",
-        age: 654,
-        color: "blue",
-    },
-    {
-        id: 12,
-        name: "agdabel",
-        age: 654,
-        color: "blue",
-    },
-]
+// const licorne = [
+//     {
+//         id: 1,
+//         name: "lola",
+//         age: 254,
+//         color: "rose",
+//     },
+//     {
+//         id: 2,
+//         name: "paola",
+//         age: 196,
+//         color: "rose",
+//     },
+//     {
+//         id: 3,
+//         name: "resti",
+//         age: 170,
+//         color: "rose",
+//     },
+//     {
+//         id: 4,
+//         name: "zoe",
+//         age: 85,
+//         color: "blue",
+//     },
+//     {
+//         id: 5,
+//         name: "jouli",
+//         age: 200,
+//         color: "rose",
+//     },
+//     {
+//         id: 6,
+//         name: "fleur",
+//         age: 186,
+//         color: "rose",
+//     },
+//     {
+//         id: 7,
+//         name: "rose",
+//         age: 128,
+//         color: "blue",
+//     },
+//     {
+//         id: 8,
+//         name: "prune",
+//         age: 124,
+//         color: "blue",
+//     },
+//     {
+//         id: 9,
+//         name: "agdabel",
+//         age: 654,
+//         color: "blue",
+//     },
+//     {
+//         id: 10,
+//         name: "agdabel",
+//         age: 654,
+//         color: "blue",
+//     },
+//     {
+//         id: 11,
+//         name: "agdabel",
+//         age: 654,
+//         color: "blue",
+//     },
+//     {
+//         id: 12,
+//         name: "agdabel",
+//         age: 654,
+//         color: "blue",
+//     },
+// ]
 
-const welcome = (req, res) => {
-    res.send("Welcome to the official Licorne institue of masachusette")
-}
-app.get("/", welcome);
+// const welcome = (req, res) => {
+//     res.send("Welcome to the official Licorne institue of masachusette")
+// }
+// app.get("/", welcome);
 
-const allLili = (req, res) => {
+// const allLili = (req, res) => {
 
-    const Reqlili = parseInt(req.query.limit)
+//     const Reqlili = parseInt(req.query.limit)
 
-    const tablili = []
-    if (Reqlili) {
-        for (i = 0; i < Reqlili; i++) {
-            tablili.push(licorne[i])
+//     const tablili = []
+//     if (Reqlili) {
+//         for (i = 0; i < Reqlili; i++) {
+//             tablili.push(licorne[i])
 
-        }
-        res.send({ tablili })
-    } else {
+//         }
+//         res.send({ tablili })
+//     } else {
 
-        res.send(licorne.slice(0, 10))
-    }
-}
+//         res.send(licorne.slice(0, 10))
+//     }
+// }
 
-app.get("/all", allLili);
+// app.get("/all", allLili);
 
+// const paramLico = (req, res) => {
 
+//     const parali = (req.query.color)
+//     const papililo = licorne.filter((el) => el.color === parali)
 
+//     if (papililo) {
+//         res.json({ papililo })
+//     } else {
+//         res.send("cette couleur n'existe pas! inculte des licorne va!")
+//     }
+// };
+// app.get("/color", paramLico)
 
+// const licorneId = (req, res) => {
+//     const licoId = parseInt(req.params.id)
+//     const licoCo = licorne.find((el) => el.id === licoId)
 
+//     if (licoCo) {
+//         res.status(200).json({ licoCo })
+//     } else {
+//         res.status(404).send("Cette licorne n'existe pas")
+//     }
+// };
+// app.get("/:id", licorneId)
 
-const paramLico = (req, res) => {
+// app.listen(port, (err) => {
 
-    const parali = (req.query.color)
-    const papililo = licorne.filter((el) => el.color === parali)
+//     if (err) {
 
-    if (papililo) {
-        res.json({ papililo })
-    } else {
-        res.send("cette couleur n'existe pas! inculte des licorne va!")
-    }
-};
-app.get("/color", paramLico)
+//         console.error("Something bad happened");
 
+//     } else {
 
+//         console.log(`Server is listening on ${port}`);
 
+//     }
 
-const licorneId = (req, res) => {
-    const licoId = parseInt(req.params.id)
-    const licoCo = licorne.find((el) => el.id === licoId)
-
-    if (licoCo) {
-        res.status(200).json({ licoCo })
-    } else {
-        res.status(404).send("Cette licorne n'existe pas")
-    }
-};
-app.get("/:id", licorneId)
-
-
-app.listen(port, (err) => {
-
-    if (err) {
-
-        console.error("Something bad happened");
-
-    } else {
-
-        console.log(`Server is listening on ${port}`);
-
-    }
-
-});
+// });
